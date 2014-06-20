ac194b884d0c1a7f262f61e1546dcd3737a6dc00
dana-laboratory <rafzalan@gmail.com>
2014-06-20 23:43:05 +0430

    edit peng robinson

--- PengRobinson.jl/src/PengRobinson.jl
+++ PengRobinson.jl/src/PengRobinson.jl
@@ -1,11 +1,47 @@
-P=0.00004*R*(25000.*Pc*T*(−1.*b^2. + 2.*b*v + v^2.) + 11431.*R*Tc*(T*k^2. + Tc - 2.*Tc*k*(T / Tc^1.)^0.5 + 2.*Tc*k - 2.*Tc*k^2.*(T / Tc^1.)^0.5 + Tc*k^2.)*(b - 1.*v)) / (Pc^1.*(b^3. - 3.*b^2.*v + b*v^2. + v^3.)^1.);
-PTv=-(((11431*k^2*Tc*v-11431*b*k^2*Tc)*R^2+(-25000*Pc*v^2-50000*b*Pc*v-25000*b^2*Pc)*R)*sqrt(T)+sqrt(Tc)*((-11431*k^2-11431*k)*Tc*v+(11431*b*k^2+11431*b*k)*Tc)*R^2)/((25000*Pc*v^3+25000*b*Pc*v^2-75000*b^2*Pc*v+25000*b^3*Pc)*sqrt(T));
-
-# s calculation REF[1] EQ(5.31)
-PTvIv=(R*(Sqrt[2]*(50000*b*Pc*Sqrt[T] - 11431*k*R*(-(k*Sqrt[T]) + Sqrt[Tc] + k*Sqrt[Tc])* Tc)*ArcTanh[(b + v)/(Sqrt[2]*b)] + 25000*b*Pc*Sqrt[T]*(4*Log[-b + v] - Log[-b^2 + 2*b*v + v^2])))/(50000*b*Pc*Sqrt[T]);
-
-# u calculation REF[1] EQ(5.36)
-TmuPTvmiPIV=-(P*v) + (R*Sqrt[T]*(50000*b*Pc*Sqrt[T] + 11431*k^2*R*Sqrt[T]*Tc - 11431*k*R*Tc^(3/2) - 11431*k^2*R*Tc^(3/2))*ArcTanh[ (b + v)/(Sqrt[2]*b)])/(25000*Sqrt[2]*b*Pc) + 2*R*T*Log[-b + v] - (R*T*Log[-b^2 + 2*b*v + v^2])/2;
-
-PvT=-(((-11431*k^2*Tc*v^3+11431*b*k^2*Tc*v^2+11431*b^2*k^2*Tc*v-11431*b^3*k^2*Tc)*R^2+(12500*Pc*v^4+50000*b*Pc*v^3+100000*b^2*Pc*v^2-62500*b^4*Pc)*R)*T+sqrt(Tc)*((22862*k^2+22862*k)*Tc*v^3+(-22862*b*k^2-22862*b*k)*Tc*v^2+(-22862*b^2*k^2-22862*b^2*k)*Tc*v+(22862*b^3*k^2+22862*b^3*k)*Tc)*R^2*sqrt(T)+((-11431*k^2-22862*k-11431)*Tc^2*v^3+(11431*b*k^2+22862*b*k+11431*b)*Tc^2*v^2+(11431*b^2*k^2+22862*b^2*k+11431*b^2)*Tc^2*v+(-11431*b^3*k^2-22862*b^3*k-11431*b^3)*Tc^2)*R^2)/(12500*Pc*v^6+25000*b*Pc*v^5-62500*b^2*Pc*v^4-50000*b^3*Pc*v^3+137500*b^4*Pc*v^2-75000*b^5*Pc*v+12500*b^6*Pc);
-PvTIT=(R*(b + v)*(11431*b^2*(1 + k)^2*R*Tc^2*T - 22862*b*(1 + k)^2*R*Tc^2*v*T + 11431*(1 + k)^2*R* Tc^2*v^2*T - (45724*b^2*k*(1 + k)*R*Tc^(3/2)* T^(3/2))/3 + (91448*b*k*(1 + k)*R*Tc^(3/2)*v* T^(3/2))/3 - (45724*k*(1 + k)*R*Tc^(3/2)*v^2* T^(3/2))/3 + 31250*b^3*Pc*T^2 + (b^2*(11431*k^2*R*Tc - 62500*Pc*v)*T^2)/2 + (v^2*(11431*k^2*R*Tc - 12500*Pc*v)*T^2)/2 - b*v*(11431*k^2*R*Tc + 18750*Pc*v)*T^2))/ (12500*Pc*(b^3 - 3*b^2*v + b*v^2 + v^3)^2);
+# REF[1] Engineering and Chemical Thermodynamics, 2nd Edition, Milo D. Koretsky
+module PengRobinson
+  #Units J,Kmol,Kelvin,pascal
+  using DanaTypes
+  export DANAPengRobinson,setEquationFlow
+  type  DANAPengRobinson <: DanaModel
+      DANAPengRobinson()=begin
+        new(8314.4621,"",NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,
+          [
+			:(P=0.00004*R*(25000.*Pc*T*(−1.*b^2.+2.*b*v+v^2.)+11431.*R*Tc*(T*k^2.+Tc-2.*Tc*k*(T/Tc^1.)^0.5+2.*Tc*k-2.*Tc*k^2.*(T/Tc^1.)^0.5+Tc*k^2.)*(b-1.*v))/(Pc^1.*(b^3.-3.*b^2.*v+b*v^2.+v^3.)^1.)),
+			:(PTv=-(((11431*k^2*Tc*v-11431*b*k^2*Tc)*R^2+(-25000*Pc*v^2-50000*b*Pc*v-25000*b^2*Pc)*R)*sqrt(T)+sqrt(Tc)*((-11431*k^2-11431*k)*Tc*v+(11431*b*k^2+11431*b*k)*Tc)*R^2)/((25000*Pc*v^3+25000*b*Pc*v^2-75000*b^2*Pc*v+25000*b^3*Pc)*sqrt(T))),
+			#s calculation REF[1] EQ(5.31)
+			:(PTvIv=(R*(Sqrt[2]*(50000*b*Pc*Sqrt[T]-11431*k*R*(-(k*Sqrt[T])+Sqrt[Tc]+k*Sqrt[Tc])*Tc)*ArcTanh[(b+v)/(Sqrt[2]*b)]+25000*b*Pc*Sqrt[T]*(4*Log[-b+v]-Log[-b^2+2*b*v+v^2])))/(50000*b*Pc*Sqrt[T])),
+			#u calculation REF[1] EQ(5.36)
+			:(TmuPTvmiPIV=-(P*v)+(R*Sqrt[T]*(50000*b*Pc*Sqrt[T]+11431*k^2*R*Sqrt[T]*Tc-11431*k*R*Tc^(3/2)-11431*k^2*R*Tc^(3/2))*ArcTanh[(b+v)/(Sqrt[2]*b)])/(25000*Sqrt[2]*b*Pc)+2*R*T*Log[-b+v]-(R*T*Log[-b^2+2*b*v+v^2])/2),
+			:(PvT=-(((-11431*k^2*Tc*v^3+11431*b*k^2*Tc*v^2+11431*b^2*k^2*Tc*v-11431*b^3*k^2*Tc)*R^2+(12500*Pc*v^4+50000*b*Pc*v^3+100000*b^2*Pc*v^2-62500*b^4*Pc)*R)*T+sqrt(Tc)*((22862*k^2+22862*k)*Tc*v^3+(-22862*b*k^2-22862*b*k)*Tc*v^2+(-22862*b^2*k^2-22862*b^2*k)*Tc*v+(22862*b^3*k^2+22862*b^3*k)*Tc)*R^2*sqrt(T)+((-11431*k^2-22862*k-11431)*Tc^2*v^3+(11431*b*k^2+22862*b*k+11431*b)*Tc^2*v^2+(11431*b^2*k^2+22862*b^2*k+11431*b^2)*Tc^2*v+(-11431*b^3*k^2-22862*b^3*k-11431*b^3)*Tc^2)*R^2)/(12500*Pc*v^6+25000*b*Pc*v^5-62500*b^2*Pc*v^4-50000*b^3*Pc*v^3+137500*b^4*Pc*v^2-75000*b^5*Pc*v+12500*b^6*Pc)),
+			:(PvTIT=(R*(b+v)*(11431*b^2*(1+k)^2*R*Tc^2*T-22862*b*(1+k)^2*R*Tc^2*v*T+11431*(1+k)^2*R*Tc^2*v^2*T-(45724*b^2*k*(1+k)*R*Tc^(3/2)*T^(3/2))/3+(91448*b*k*(1+k)*R*Tc^(3/2)*v*T^(3/2))/3-(45724*k*(1+k)*R*Tc^(3/2)*v^2*T^(3/2))/3+31250*b^3*Pc*T^2+(b^2*(11431*k^2*R*Tc-62500*Pc*v)*T^2)/2+(v^2*(11431*k^2*R*Tc-12500*Pc*v)*T^2)/2-b*v*(11431*k^2*R*Tc+18750*Pc*v)*T^2))/(12500*Pc*(b^3-3*b^2*v+b*v^2+v^3)^2)),
+			:(b=0.0778*R*Tc/Pc),
+			:(k=0.37464+1.54226*af-0.26992*af^2)
+		  ],Array(Expr,0)
+        )
+      end
+      #paremeters
+      R::Float64
+      CASNO::String
+      #variables
+      v::Float64
+      T::Float64
+      Tc::Float64
+      P::Float64
+      Pc::Float64
+      PTv::Float64
+      PTvIv::Float64
+      TmuPTvmiPIV::Float64
+      PvT::Float64
+      PvTIT::Float64
+	  k::Float64
+	  b::Float64
+	  af::Float64
+      #equations
+      equations::Array{Expr,1}
+      equationsFlow::Array{Expr,1}
+  end
+  function setEquationFlow(this::DANAPengRobinson)
+	equationsFlow=equations;
+  end
+end
